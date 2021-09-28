0d5843526a92d66a8e478fa3e38c649cee984278
Artori-kun <rezo.krifferson@gmail.com>
2021-09-28 10:13:19 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# Voicebot Interface
+
+This is a duplicate from [Rasa Voice Interface](https://github.com/RasaHQ/rasa-voice-interface) with some minor modifications to work with my Rasa-based Vietnamese [voice assistant](https://github.com/Artori-kun/rasa_voicebot).
+
---
+++ src/main.js
@@ -0,0 +1,51 @@
+import Vue from 'vue';
+import VueSocketIO from 'vue-socket.io-extended';
+import socket from 'socket.io-client';
+import $ from 'jquery';
+import fontawesome from '@fortawesome/fontawesome';
+import solid from '@fortawesome/fontawesome-free-solid';
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import store from './store';
+import router from './router';
+import App from './App.vue';
+import MessageHandlerMixin from './mixins/MessageHandlerMixin';
+
+Vue.mixin(MessageHandlerMixin);
+global.$ = $;
+
+/* FontAwesome */
+fontawesome.library.add(solid);
+Vue.component('font-awesome-icon', FontAwesomeIcon);
+
+// Vue.use(
+// 	new VueSocketIO({
+// 		debug: true,
+// 		connection: 'http://localhost:5005',
+// 		vuex: {
+// 			store,
+// 			actionPrefix: 'SOCKET_',
+// 			mutationPrefix: 'SOCKET_'
+// 		}
+// 		// transports: ['websocket', 'polling', 'flashsocket']
+// 	})
+// );
+
+const ioInstance = socket('http://localhost:5005', {
+	reconnection: true,
+	reconnectionDelay: 500,
+	maxReconnectionAttempts: Infinity
+});
+Vue.use(VueSocketIO, ioInstance, {
+	store, // vuex store instance
+	actionPrefix: 'SOCKET_', // keep prefix in uppercase
+	mutationPrefix: 'SOCKET_'
+});
+
+/* App Mount */
+Vue.config.productionTip = false;
+
+new Vue({
+	router,
+	store,
+	render: h => h(App)
+}).$mount('#app');
